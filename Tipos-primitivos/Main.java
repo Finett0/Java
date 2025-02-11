4704d7aca748398d5b3c8e9555ad297da9d2106c
Giovanni Finetto <118924567+Finett0@users.noreply.github.com>
2025-02-11 20:14:24 -0300

    Create Main.java

---
+++ Tipos-primitivos/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        int idade = 25;
+        double salarioMensal = 3800.00;
+        char genero = 'M';
+        boolean empregadoJava = true;
+
+        System.out.println("Idade: " + idade);
+        System.out.println("Salário: " + salarioMensal);
+        System.out.println("Gemero: " + genero);
+        System.out.print("Empregado: " + empregadoJava);
+
+
+
+
+    }
+}
