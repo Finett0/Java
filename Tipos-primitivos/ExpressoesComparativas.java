cda34739a5619fb43df3e7bc2d269425add9dd1b
Giovanni Finetto <118924567+Finett0@users.noreply.github.com>
2025-02-13 14:46:48 -0300

    Create ExpressoesComparativas.java

---
+++ Tipos-primitivos/ExpressoesComparativas.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class ExpressoesComparativas {
+    public static void main (String[] args){
+
+        int pao = 10;
+        int acucar = 20;
+        System.out.println(pao == acucar);
+        System.out.println(pao != acucar);
+        System.out.println(pao > acucar);
+        System.out.println(pao < acucar);
+        System.out.println(pao >= acucar);
+        System.out.println(pao <= acucar);
+
+
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("Digite o primeiro numero: ");
+//        Integer numA = scanner.nextInt();
+//        System.out.println("Digite o segundo numero: ");
+//        Integer numB = scanner.nextInt();
+//        boolean diferente = numA != numB;
+//        System.out.println("O numero " + numA + " é diferente do numero " + numB + ": " + diferente );
+
+    }
+}
